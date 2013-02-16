640cdd0dfc5f6c1bb4ec8f9deb0b55365c90a031
jlarkcentral <feral@feral.(none)>
2013-02-16 03:16:26 +0100

    centrer toudou : ca m enervait ><

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QtGui/QApplication>
+#include <QDesktopWidget>
 
 #include "firstwindow.h"
 
@@ -8,6 +9,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     //MainWindow w;
     FirstWindow w;
+    // centrer la fenetre (il y a p-e un moyen plus propre)
+    w.move((QApplication::desktop()->width() - w.width())/2, (QApplication::desktop()->height() - w.height())/2);
     w.show();
 
     return a.exec();
